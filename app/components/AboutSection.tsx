1b98a99a6309a0d0d0a06cbed01dc287e56d5209
Ruben Buentello <ruben_015@Rubens-MacBook-Air-2.local>
2025-11-20 01:16:15 -0600

    Goated Website

--- app/components/AboutSection.tsx
+++ app/components/AboutSection.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 import TabButton from "./TabButton";
 
 const SkillCard = ({ title, skills, color }: { title: string, skills: string[], color: string }) => (
-  <div className={`bg-[#181818] p-4 rounded-lg hover:bg-[#232323] transition-all duration-300`}>
+  <div className="bg-[#181818] p-4 rounded-lg hover:bg-[#232323] transition-all duration-300">
     <h3 className={`text-xl font-semibold mb-2 text-${color}-500`}>{title}</h3>
     <ul className="list-none space-y-2">
       {skills.map((skill, index) => (
@@ -30,23 +30,23 @@ const TAB_DATA = [
     content: (
       <div className="grid grid-cols-2 md:grid-cols-4 gap-4">
         <SkillCard 
-          title="Development" 
-          skills={["Node.js", "Next.js", "React", "JavaScript"]}
+          title="JavaScript" 
+          skills={["JavaScript"]}
           color="cyan"
         />
         <SkillCard 
-          title="Game Dev" 
-          skills={["Unity", "Godot", "C#", "C++"]}
+          title="Java" 
+          skills={["Java"]}
           color="purple"
         />
         <SkillCard 
-          title="AI & ML" 
-          skills={["Python", "OpenCV", "Deep Learning", "LLM"]}
+          title="HTML" 
+          skills={["HTML"]}
           color="green"
         />
         <SkillCard 
-          title="Other" 
-          skills={["PostgreSQL", "Git", "GCP", "VR/AR"]}
+          title="Godot" 
+          skills={["Godot"]}
           color="yellow"
         />
       </div>
@@ -58,29 +58,14 @@ const TAB_DATA = [
     content: (
       <div className="flex flex-col space-y-4">
         <div className="bg-[#181818] p-6 rounded-lg border border-gray-800">
-          <div className="flex flex-col space-y-4">
-            {/* Masters Degree */}
-            <div className="flex flex-col space-y-2">
-              <h3 className="text-xl font-bold text-white">
-                Master&apos;s in Computer Science
-              </h3>
-              <p className="text-gray-300">University of Texas Rio Grande Valley</p>
-              <div className="flex items-center space-x-2">
-                <div className="h-1 w-1 rounded-full bg-gray-400"></div>
-                <span className="text-sm text-gray-400">Graduate Studies</span>
-              </div>
-            </div>
-
+          <div className="flex flex-col space-y-4">            
             {/* Bachelors Degree */}
             <div className="flex flex-col space-y-2">
               <h3 className="text-xl font-bold text-white">
-                Bachelor&apos;s in Computer Science
+                Diploma
               </h3>
-              <p className="text-gray-300">University of Texas Rio Grande Valley</p>
-              <div className="flex items-center space-x-2">
-                <div className="h-1 w-1 rounded-full bg-gray-400"></div>
-                <span className="text-sm text-gray-400">Undergraduate Studies</span>
-              </div>
+              <p className="text-gray-300">Sharyland Pioneer High School</p>
+              <div className="flex items-center space-x-2"></div>
             </div>
           </div>
         </div>
@@ -96,28 +81,28 @@ const TAB_DATA = [
           icon={<svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24">
             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M11 4a2 2 0 114 0v1a1 1 0 001 1h3a1 1 0 011 1v3a1 1 0 01-1 1h-1a2 2 0 100 4h1a1 1 0 011 1v3a1 1 0 01-1 1h-3a1 1 0 01-1-1v-1a2 2 0 10-4 0v1a1 1 0 01-1 1H7a1 1 0 01-1-1v-3a1 1 0 00-1-1H4a2 2 0 100 4h1a1 1 0 011 1v3a1 1 0 01-1 1H2a1 1 0 01-1-1v-3a1 1 0 011-1h1a2 2 0 100-4H2a1 1 0 01-1-1V7a1 1 0 011-1h3a1 1 0 001-1V4z" />
           </svg>}
-          text="Game Development"
-          color="purple"
+          text="Playing French Horn"
+          color="orange"
         />
         <HobbyCard 
           icon={<svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24">
             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 6.253v13m0-13C10.832 5.477 9.246 5 7.5 5S4.168 5.477 3 6.253v13C4.168 18.477 5.754 18 7.5 18s3.332.477 4.5 1.253m0-13C13.168 5.477 14.754 5 16.5 5c1.747 0 3.332.477 4.5 1.253v13C19.832 18.477 18.247 18 16.5 18c-1.746 0-3.332.477-4.5 1.253" />
           </svg>}
           text="Reading Manga/Manhwa"
-          color="red"
+          color="blue"
         />
         <HobbyCard 
           icon={<svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24">
             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15 10l4.553-2.276A1 1 0 0121 8.618v6.764a1 1 0 01-1.447.894L15 14M5 18h8a2 2 0 002-2V8a2 2 0 00-2-2H5a2 2 0 00-2 2v8a2 2 0 002 2z" />
           </svg>}
           text="Watching Animated Shows"
-          color="blue"
+          color="red"
         />
         <HobbyCard 
           icon={<svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24">
             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 6v6m0 0v6m0-6h6m-6 0H6" />
           </svg>}
-          text="Cooking and Baking"
+          text="Running"
           color="yellow"
         />
         <HobbyCard 
@@ -125,14 +110,14 @@ const TAB_DATA = [
             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15 5v2m0 4v2m0 4v2M5 5a2 2 0 00-2 2v3a2 2 0 110 4v3a2 2 0 002 2h14a2 2 0 002-2v-3a2 2 0 110-4V7a2 2 0 00-2-2H5z" />
           </svg>}
           text="Playing Video Games"
-          color="green"
+          color="purple"
         />
         <HobbyCard 
           icon={<svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24">
             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z" />
           </svg>}
-          text="Drawing"
-          color="pink"
+          text="Music"
+          color="green"
         />
       </div>
     ),
@@ -221,29 +206,26 @@ const AboutSection = () => {
           <div className="space-y-8">
             <p className="text-base lg:text-lg leading-relaxed">
               <span className="block text-gray-300 hover:text-white transition-colors duration-300">
-                I am a Graduate Research Assistant at 
-                <span className="text-cyan-400 font-medium hover:text-cyan-300"> ASARG </span>
-                (Algorithmic Self-Assembly Research Group) and a part-time developer at 
-                <span className="text-purple-400 font-medium hover:text-purple-300"> MVSSIVE</span>, 
-                a music technology company.
+                I am a computer science student at 
+                <span className="text-cyan-400 font-medium hover:text-cyan-300"> UTRGV </span>
+                who loves to play the French horn. I&apos;m passionate about both technology and music, 
+                and I enjoy combining my creative and technical interests in unique ways.
               </span>
             </p>
             
             <p className="text-base lg:text-lg leading-relaxed">
               <span className="block text-gray-300 hover:text-white transition-colors duration-300">
-                As a former instructor for 
-                <span className="text-green-400 font-medium hover:text-green-300"> CSCI 1101</span>, 
-                I had the privilege of introducing computer science fundamentals to new students, 
-                hoping to inspire the next generation of developers.
+                My studies focus on various aspects of computer science, including software development, 
+                algorithms, and modern web technologies. I&apos;m always eager to learn new programming 
+                languages and frameworks to expand my skill set.
               </span>
             </p>
 
             <p className="text-base lg:text-lg leading-relaxed">
               <span className="block text-gray-300 hover:text-white transition-colors duration-300">
-                My passion lies in theoretical computer science, and I&apos;m actively pursuing my goal 
-                of obtaining a PhD in this field. Alongside my academic pursuits, I&apos;m also an avid 
-                game developer, having created various games and projects that combine my love for 
-                programming with creative expression.
+                When I&apos;m not coding, you can find me practicing my French horn or exploring 
+                new hobbies. I believe in maintaining a balance between my academic pursuits and 
+                personal interests, as both contribute to my growth as a well-rounded individual.
               </span>
             </p>
             
